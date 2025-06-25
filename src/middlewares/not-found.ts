71ecee8397c2183e2657d430403f809dd994b14b
hattaghazali <hattaltd@gmail.com>
2025-06-25 19:08:09 +0800

    FEAT: Add not found api route

---
+++ src/middlewares/not-found.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from "express";
+
+// 404 Not Found handler
+export const notFoundHandler = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  res.status(404).json({
+    message: "API Route Not Found",
+    method: req.method,
+    path: req.originalUrl,
+  });
+};
