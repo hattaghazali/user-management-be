f337e6291d18f9ddfda604dc1baf27e4dee9f51a
hattaghazali <hattaltd@gmail.com>
2025-06-27 03:55:35 +0800

    FEAT: Add user model

---
+++ src/models/model-user.ts
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from 'mongoose';
+import {
+    EGender,
+    EOccupation,
+    EState,
+    EStatus,
+    IUser,
+} from '../types/type-user';
+
+const userSchema = new Schema<IUser>(
+    {
+        u_email: { type: String, required: true },
+        u_password: { type: String, required: true },
+        u_name: { type: String },
+        u_gender: { type: Number, enum: EGender },
+        u_occupation: { type: Number, enum: EOccupation },
+        u_state: { type: Number, enum: EState },
+        u_status: { type: Number, enum: EStatus },
+    },
+    { timestamps: true }
+);
+
+export const User = mongoose.model<IUser>('tbl_users', userSchema);
