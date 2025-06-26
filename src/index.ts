620c360366ff816a926de5243349900116161c89
hattaghazali <hattaltd@gmail.com>
2025-06-27 03:54:09 +0800

    REFACTOR: Minor adjustment on the codes

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
-import express, { Application, NextFunction, Request, Response } from "express";
+import express, { Application } from 'express'
+import connectDB from './configs/connect-db'
+import configs from './configs/configs'
+import route_admin from './routes/route-admin'
+import { notFoundHandler } from './middlewares/not-found'
 
-import configs from "./configs/configs";
-import connectDB from "./configs/connect-db";
-import { notFoundHandler } from "./middlewares/not-found";
-import basic from "./routes/route-basic";
-
-const app: Application = express();
+const app: Application = express()
 
 // START: Initialize MongoDB connection
-connectDB();
+connectDB()
 
 // START: Middleware
+app.use(express.json())
 
 // START: Routes
-app.use("/api/user", basic);
+app.use('/api/user', route_admin)
 
 // START: Route not found
-app.use(notFoundHandler);
+app.use(notFoundHandler)
 
 // START: Server
 app.listen(configs.port, async () => {
-  console.log(`LOG: Server started on PORT:::${configs.port} ${configs.bruh}`);
-});
+    console.log(`LOG: Server started on PORT:::${configs.port} ${configs.bruh}`)
+})
