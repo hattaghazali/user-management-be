6dbeaab2a0ade7f9e4f455e1cad300cae8f6900c
hattaghazali <hattaltd@gmail.com>
2025-06-27 03:55:01 +0800

    CHORE: Remove this file

--- src/routes/route-basic.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getListOfUsers } from "../controllers/controller-basic";
-
-// START BASIC ROUTING
-const router: Router = Router();
-
-router.get("/list-users", getListOfUsers);
-
-export default router;
