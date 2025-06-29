21ef080fa4215dd63c678338b26d7e29d61ee046
hattaghazali <hattaltd@gmail.com>
2025-06-30 03:29:13 +0800

    REFACTOR: Adjust config db uri

--- src/configs/configs.ts
+++ src/configs/configs.ts
@@ -28,7 +28,7 @@ export const MONGO_USER = process.env.MONGO_USER!;
 export const MONGO_PASSWORD = process.env.MONGO_PASSWORD!;
 export const MONGO_DB_NAME = process.env.MONGO_DB_NAME!;
 export const MONGO_HOST = process.env.MONGO_HOST!;
-export const MONGO_URL = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}/${MONGO_DB_NAME}`;
+export const MONGO_URL = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}/${MONGO_DB_NAME}?retryWrites=true`;
 
 const configs: IConfig = {
     bruh: BRUH,
