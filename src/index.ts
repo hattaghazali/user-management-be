e5adf89893442179fc770ceb3dbf233b8121faf7
hattaghazali <hattaltd@gmail.com>
2025-06-29 19:36:39 +0800

    REFACTOR: const naming

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
-import express, { Application } from 'express'
-import connectDB from './configs/connect-db'
-import configs from './configs/configs'
-import route_admin from './routes/route-admin'
-import { notFoundHandler } from './middlewares/not-found'
+import express, { Application } from 'express';
+import connectDB from './configs/connect-db';
+import configs from './configs/configs';
+import route_admin from './routes/route-admin';
+import { routeNotFound } from './middlewares/mware-notfound';
 
-const app: Application = express()
+const app: Application = express();
 
-// START: Initialize MongoDB connection
-connectDB()
+// START: MongoDB
+connectDB();
 
 // START: Middleware
-app.use(express.json())
+app.use(express.json());
 
 // START: Routes
-app.use('/api/user', route_admin)
-
-// START: Route not found
-app.use(notFoundHandler)
+app.use('/api/admin', route_admin);
+app.use(routeNotFound);
 
 // START: Server
 app.listen(configs.port, async () => {
-    console.log(`LOG: Server started on PORT:::${configs.port} ${configs.bruh}`)
-})
+    console.log('------------------------------------------------------------------------------');
+    console.log(`[LOG] Server started. URL: ${configs.api_base}:${configs.port}`);
+    console.log(`[LOG] Using environment. ENV: ${configs.bruh}`);
+});
