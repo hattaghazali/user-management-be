dcee32f920d2f16ad0a87e3d7418725512821d37
hattaghazali <hattaltd@gmail.com>
2025-07-23 21:50:49 +0800

    BUILD: Run build for prod

--- src/configs/connect-db.ts
+++ src/configs/connect-db.ts
@@ -3,16 +3,16 @@ import { CONST_MONGO_URL } from './constants';
 
 const connectDB = async () => {
     try {
-        if (mongoose.connection.readyState === 1) {
-            console.log('[LOG] Already connected to MongoDB.');
-            return;
-        }
+        // if (mongoose.connection.readyState === 1) {
+        //     console.log('[LOG] Already connected to MongoDB.');
+        //     return;
+        // }
 
-        if (mongoose.connection.readyState === 2) {
-            console.log('[LOG] Connection in progress, waiting...');
-            await mongoose.connection.asPromise();
-            return mongoose.connection;
-        }
+        // if (mongoose.connection.readyState === 2) {
+        //     console.log('[LOG] Connection in progress, waiting...');
+        //     await mongoose.connection.asPromise();
+        //     return mongoose.connection;
+        // }
 
         const db_connection = await mongoose.connect(CONST_MONGO_URL, {
             connectTimeoutMS: 10000,
