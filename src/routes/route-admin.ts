d5bcc2478eeb0ca629995110b4e311b86abd80e2
hattaghazali <hattaltd@gmail.com>
2025-08-11 07:30:22 +0800

    REFACTOR: Add some new routes

--- src/routes/route-admin.ts
+++ src/routes/route-admin.ts
@@ -5,9 +5,13 @@ import {
     adminGetUsers,
     adminGetUserDemographics,
     adminGetAUser,
+    adminEditAUserInfo,
     adminGetInfo,
+    adminEditAUserImage,
+    adminUploadUserPicture,
 } from '../controllers/controller-admin';
 import verifyToken from '../middlewares/mware-vertifytoken';
+import { upload } from '../configs/cloudinary';
 
 // START: Admin Routes
 const router: Router = Router();
@@ -17,6 +21,9 @@ router.get('/demographics', verifyToken, adminGetUserDemographics);
 router.get('/user/list-users', verifyToken, adminGetUsers);
 router.get('/user/list-users-v2', adminGetUsers);
 router.get('/user/:id', verifyToken, adminGetAUser);
+router.patch('/user/edit/:id', verifyToken, adminEditAUserInfo);
 router.post('/user/register-user', verifyToken, adminRegisterAUser);
+router.put('/user/edit-image/:id', verifyToken, upload.single('pictureFile'), adminEditAUserImage);
+router.post('/user/upload-picture-test', upload.single('pictureFile'), adminUploadUserPicture);
 
 export default router;
