a62901a46156d13bcc53180f3f29d122f7ec08fd
hattaghazali <hattaltd@gmail.com>
2025-08-11 07:31:54 +0800

    REFACTOR: Adjust index

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { CONST_API_BASE, CONST_BRUH, CONST_SERVER_PORT } from './configs/constan
 import { routeNotFound } from './middlewares/mware-notfound';
 import corsOptions from './middlewares/mware-cors';
 import routeAdmin from './routes/route-admin';
+import { MulterError } from 'multer';
 
 const app: Application = express();
 
@@ -22,6 +23,28 @@ app.use(express.json());
 // START: Routes
 app.use('/api/admin', routeAdmin);
 app.use(routeNotFound);
+
+// Global error-handling middleware (must be after all routes)
+// app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+//     // Handle Multer-specific errors and fileFilter errors
+//     if (
+//         err instanceof MulterError ||
+//         err.message === 'Unsupported file format. Only JPG and PNG are allowed.'
+//     ) {
+//         res.status(400).json({
+//             success: false,
+//             message: err.message || 'Invalid file upload request.',
+//         });
+//         return;
+//     }
+
+//     // Handle other errors
+//     res.status(err.status || 500).json({
+//         success: false,
+//         message: err.message || 'An unexpected error occurred.',
+//     });
+//     return;
+// });
 
 // START: Server
 app.listen(CONST_SERVER_PORT, async () => {
