b20d6892f6fae357bed5f678fbdd056b869b8685
hattaghazali <hattaltd@gmail.com>
2025-08-11 07:29:05 +0800

    REFACTOR: Adjust model of user

--- src/models/model-user.ts
+++ src/models/model-user.ts
@@ -3,13 +3,26 @@ import { EGender, EOccupation, EState, EStatus, IUser } from '../types/type-user
 
 const userSchema = new Schema<IUser>(
     {
-        u_email: { type: String, required: true },
-        u_password: { type: String, required: true },
-        u_name: { type: String },
-        u_gender: { type: Number, enum: EGender },
-        u_occupation: { type: Number, enum: EOccupation },
-        u_state: { type: Number, enum: EState },
-        u_status: { type: Number, enum: EStatus },
+        email: { type: String, required: true },
+        password: { type: String, required: true },
+        name: { type: String },
+        gender: { type: Number, enum: EGender },
+        occupation: { type: Number, enum: EOccupation },
+        state: { type: Number, enum: EState },
+        status: { type: Number, enum: EStatus },
+        avatar: {
+            public_id: {
+                type: String,
+                required: true,
+                default: 'user-pictures/dxoyhanhlve7j818kusy',
+            },
+            img_url: {
+                type: String,
+                required: true,
+                default:
+                    'https://res.cloudinary.com/hattacloudinary/image/upload/v1754327277/user-pictures/dxoyhanhlve7j818kusy.jpg',
+            },
+        },
     },
     { timestamps: true, collection: 'User' }
 );
