108d7763f5857eaf0f2385bf86bb710734fca73f
hattaghazali <hattaltd@gmail.com>
2025-08-18 15:06:44 +0800

    BUILD: Generate cors middleware

--- dist/middlewares/mware-cors.js
+++ dist/middlewares/mware-cors.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const corsOptions = {
-    origin: ['https://user-management-fe-psi.vercel.app/', 'http://localhost:5173'], // MatchING FE's address
+    origin: ['https://user-management-fe-psi.vercel.app', 'http://localhost:5173'], // MatchING FE's address
     methods: ['GET', 'POST', 'PATCH', 'PUT'], // Specify the allowed HTTP methods
     credentials: true,
 };
