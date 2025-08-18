aa5f0e40001b801a25762f1e38ef5fec4a99312c
hattaghazali <hattaltd@gmail.com>
2025-08-18 15:07:01 +0800

    REFACTOR: Adjust origin

--- src/middlewares/mware-cors.ts
+++ src/middlewares/mware-cors.ts
@@ -1,5 +1,5 @@
 const corsOptions = {
-    origin: ['https://user-management-fe-psi.vercel.app/', 'http://localhost:5173'], // MatchING FE's address
+    origin: ['https://user-management-fe-psi.vercel.app', 'http://localhost:5173'], // MatchING FE's address
     methods: ['GET', 'POST', 'PATCH', 'PUT'], // Specify the allowed HTTP methods
     credentials: true,
 };
